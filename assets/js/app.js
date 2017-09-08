cb5c07e20b1b63157eded4ecb9833e33bd7109d3
Michael Fox <michaelscottfox1@gmail.com>
2017-09-07 19:10:59 -0700

    button generation && maryland infoWindow

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,10 +27,25 @@ var options = {
 }
 
 var dataArray = [];
+var years = [];
 
 
 
 //****************FUNCTIONS****************//
+
+function makeTimeButtons(){
+  $('#timeButtons').empty();
+  years.sort();
+
+  for(var i=0; i<years.length; i++){
+    var yearNum = parseInt(years[i]);
+    var newBtn = $('<button>');
+    newBtn.text(years[i]);
+    newBtn.attr('data-year', yearNum);
+    newBtn.addClass('btn btn-warning timeBtn');
+    $('#timeButtons').append(newBtn);
+  }
+}
 
 $('#drugTest').on("click", function(){
   var queryURL = 'https://data.maryland.gov/resource/ryrr-nv83.json';
@@ -42,10 +57,62 @@ $('#drugTest').on("click", function(){
     dataArray = [];
     for(var i=0; i< data.length; i++){
       dataArray.push(data[i]);
+      years.push(data[i].calendar_year);
     }
-    console.log(dataArray);
+
+    makeTimeButtons();
   });
+
+
+  initMarylandMap();
+
 });
+
+function initMarylandMap() {
+  var theWire = {lat: 39.0458, lng: -76.6413};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 7,
+    center: theWire
+  });
+
+  var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">Drug Deaths in Maryland</h1>'+
+      '<div id="bodyContent">'+
+      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+      'sandstone rock formation in the southern part of the '+
+      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+      'south west of the nearest large town, Alice Springs; 450&#160;km '+
+      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+      'Aboriginal people of the area. It has many springs, waterholes, '+
+      'rock caves and ancient paintings. Uluru is listed as a World '+
+      'Heritage Site.</p>'+
+      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+      '(last visited June 22, 2009).</p>'+
+      '</div>'+
+      '</div>';
+
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+  var marker = new google.maps.Marker({
+    position: theWire,
+    map: map,
+    title: 'Uluru (Ayers Rock)'
+  });
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+}
+
+
+
+
 
 
 
