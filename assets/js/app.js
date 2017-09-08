d0c786d69c863406787cff6f71ec06550d59ad4b
Michael Fox <michaelscottfox1@gmail.com>
2017-09-07 19:38:23 -0700

    resolved conflicts, reworked repository

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,7 +29,157 @@ var options = {
 var dataArray = [];
 var years = [];
 
+/*
 
+//*****PINPOINT***********
+$("#pinpoint").on("click", function() {
+       // To find out which dataset was checked, we'll grab all radio buttons
+       var whichDataset = document.getElementsByName("datasets");
+       var len = whichDataset.length;
+
+       // Run a for loop thru them all,
+       for (var i=0; i<len; i++) {
+         // find the one that is checked,
+         if (whichDataset[i].checked) {
+           // and get the value (json url) for that dataset.
+           var dataValue = whichDataset[i].value;
+           // var dataValue will be a local variable inside the scope of the onclick
+           // functions. Vars declared and assigned in if statements and for loops fall
+           // into the scope of the parent since they are not themselves functions,
+           // but just code blocks.
+           console.log("dataValue: "+dataValue);
+         }
+       }
+
+   // Now that we know which dataset the user wants, let's proceed:
+
+     // Inital if statement for the unique dataset stored on Firebase
+     // if (dataValue!="https://data.maryland.gov/resource/ryrr-nv83.json" &&
+     //     dataValue!="https://data.cdc.gov/resource/xhcb-kq4k.json") {
+     //   curl
+     // }
+
+     // Performing an AJAX request with the queryURL aka dataValue
+     $.ajax({
+         url: dataValue,
+         method: "GET"
+       })
+       // After data comes back from the request
+       .done(function(response) {
+
+         // Store the data into the global array
+         for (var i = 0; i < response.length; i++) {
+           dataArray.push(response[i]);
+
+           // Now we have actions depending on which dataset was chosen.
+           // If the data was the DRUNK DRIVING data...
+           if (response[i].state && response[i].alcohol_impaired_driving_deaths) {
+
+             // Start by changing map options for location and zoom
+             options = {
+               zoom: 4,
+               // center of the continental USA
+               center: {lat: 39.8283,lng: -98.5795}
+               }
+
+             // Next, store values for simplicity/clarity
+             var deaths = response[i].alcohol_impaired_driving_deaths;
+             var stName = response[i].state_state;
+             // get the latitude and longitude values
+             var x = response[i].state.coordinates[1];
+             var y = response[i].state.coordinates[0];
+
+             // object for coords
+             var latLong = {coords:{lat: x,lng: y}};
+             // object for onclick pop up window
+             var deathsPopUp = {popUp:"<h4 id='pop'>"+stName+" had "+deaths+" drunk driver deaths</h4>"};
+             // object for the marker labels
+             var deathsLabel = {label:deaths};
+
+             // add each object to the json object
+             $.extend(response[i], deathsLabel, deathsPopUp, latLong);
+           }
+
+           // Else, if it the data is the DRUG DEATHS data
+           else if (response[i].calendar_year) {
+             // Start by changing map options for location and zoom
+             options = {
+               zoom: 8,
+               // Baltimore, MD
+               center: {lat: 39.2904,lng: -76.6122}
+               }
+
+
+             // Next, store values for simplicity/clarity
+             var deaths = response[i].alcohol_deaths;
+             // get the latitude and longitude values for Baltimore
+             var x = 39.2904;
+             var y = -76.6122;
+
+     // THIS WILL BE CODED BY MICHAEL. NEEDS TO BE NAVIGATABLE BY YEAR AND BY DRUG
+             // object for coords
+             var latLong = {coords:{lat: x,lng: y}};
+             // object for onclick pop up window
+             var deathsPopUp = {popUp:"<h4 id='pop'>Maryland had "+deaths+" deaths due to alcohol</h4>"};
+             // object for the marker labels
+             var deathsLabel = {label:deaths};
+
+             // add each object to the json object
+             $.extend(response[i], deathsLabel, deathsPopUp, latLong);
+           }
+
+
+           // Else, if it the data is the WW POP data
+           else {
+             // Start by changing map options for location and zoom
+             options = {
+               zoom: 2,
+               // Center of the global map
+               center: {lat: 0,lng: 0}
+               }
+
+
+
+       // 11 objects, 1 for a diff contry each
+       //   response[i].countryName
+       // start by working on the total pop values for 2014
+       //   would be great to later sort by years
+       //     response[i].popData.total.1990-2014
+       //   sort by the 3 age brackets
+       //     response[i].popData.percent_0_to_14
+       //     response[i].popData.percent_15_to_64
+       //     response[i].popData.percent_65_plus
+
+
+             // Next, store values for simplicity/clarity
+             var year = 2016;
+             var totalPop = response[i].popData.total;
+             console.log("Total Pop: "+totalPop);
+             // get the latitude and longitude values for each country
+               // Geocoding!
+
+
+             // object for coords
+             var latLong = {coords:{lat: x,lng: y}};
+             // object for onclick pop up window
+             var popPopUp = {popUp:"<h4 id='pop'>This Country's Total Pop: "+totalPop+"</h4>"};
+             // object for the marker labels
+             var popLabel = {label:totalPop};
+
+             // add each object to the json object
+             $.extend(response[i], popLabel, popPopUp, latLong);
+           }
+
+
+
+
+           console.log(response[i]);
+         }
+         initMap();
+       })
+     });
+
+*/
 
 //****************FUNCTIONS****************//
 
