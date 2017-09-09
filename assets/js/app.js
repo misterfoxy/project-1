9df86b37b31b5bdafcd639e67513c23d4658217a
Tyler Mitchell <jon87tyler@gmail.com>
2017-09-08 21:38:54 -0700

    Cleaned up index.html, style.css, app.js code.  Added notes for clarity.  Merged redundant functions in app.js. Got infoWindow for Drug Deaths in MD working.  Time Buttons being generated without duplicates.  Got markers working on WW Pop data.  Still need to get infoWindow for WW Pop and then get the Time Button functionality set up to change years of data and dynamically update the infoWindows.

--- assets/js/app.js
+++ assets/js/app.js
@@ -252,11 +252,11 @@ var year = 2014;
 
 
 
-                console.log(response[i]);
-              }
-              initMap();
-            })
-          });
+          console.log(response[i]);
+        }
+        initMap();
+      })
+    });
 
 
   // Creat buttons in bottom left to sort thru years of data
@@ -277,79 +277,6 @@ var year = 2014;
         $('#timeButtons').append(newBtn);
       }
     };
-
-
-  // Test the DRUG DEATHS IN MD 
-    $('#drugTest').on("click", function(){
-      var queryURL = 'https://data.maryland.gov/resource/ryrr-nv83.json';
-
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).done(function(response){
-        dataArray = [];
-        for(var i=0; i< response.length; i++){
-          dataArray.push(response[i]);
-          console.log("DATA ARRAY: " + dataArray);
-          if (response[i].calendar_year) {
-            years.push(response[i].calendar_year);
-            console.log("YEARS ARRAY: " + years);
-          }
-        }
-        console.log("+++++++++++++++++++++++++++++++");
-        console.log("TOTAL YEARS ARRAY: "+years);
-        console.log("+++++++++++++++++++++++++++++++");
-        // makeTimeButtons();
-      });
-      initMarylandMap();
-    });
-
-
-  // Michael's MD map
-    function initMarylandMap() {
-      var theWire = {lat: 39.0458, lng: -76.6413};
-      var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 7,
-        center: theWire
-      });
-
-      var contentString = '<div id="content">'+
-          '<div id="siteNotice">'+
-          '</div>'+
-          '<h1 id="firstHeading" class="firstHeading">Drug Deaths in Maryland</h1>'+
-          '<div id="bodyContent">'+
-          '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-          'sandstone rock formation in the southern part of the '+
-          'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-          'south west of the nearest large town, Alice Springs; 450&#160;km '+
-          '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-          'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-          'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-          'Aboriginal people of the area. It has many springs, waterholes, '+
-          'rock caves and ancient paintings. Uluru is listed as a World '+
-          'Heritage Site.</p>'+
-          '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-          'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-          '(last visited June 22, 2009).</p>'+
-          '</div>'+
-          '</div>';
-
-      var infowindow = new google.maps.InfoWindow({
-        content: contentString
-      });
-
-      var marker = new google.maps.Marker({
-        position: theWire,
-        map: map,
-        title: 'Uluru (Ayers Rock)'
-      });
-      marker.addListener('click', function() {
-        infowindow.open(map, marker);
-      });
-    }
-
-
-
 
 
 //**************GOOGLE MAP**************//
