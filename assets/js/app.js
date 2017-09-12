6994d022a15a7246ddbbb7cb1837519cc114d5fe
jtmPC <jon87tyler@gmail.com>
2017-09-11 19:04:05 -0700

    Fixed WW Pop data infoWindows, time button rendering, and time button functionaility for that dataset.

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,19 +31,24 @@ $(document).ready(function() {
 
   // It all starts with the click on the PinPoint! button...
   $("#pinpoint").on("click", function() {
+
     // Begin by emptying Time Buttons and reseting arrays
     $("#timeArea").empty();
+
     // Empty both global arrays first
     dataArray = [];
     years = [];
 
     // Now we'll grab all radio buttons to find out which dataset was checked
     var whichDataset = document.getElementsByName("datasets");
-    var len = whichDataset.length;
+
     // Run a for loop thru them all,
+    var len = whichDataset.length;
     for (var i = 0; i < len; i++) {
+
       // find the one that is checked,
       if (whichDataset[i].checked) {
+
         // and get the value (json url) for that dataset.
         var dataValue = whichDataset[i].value;
         // var dataValue will be a local variable inside the scope of the #pinpoint
@@ -52,6 +57,7 @@ $(document).ready(function() {
         // but just code blocks.
       }
     }
+
     // Now that we know which dataset the user wants, let's proceed:
     // Performing an AJAX request with the queryURL aka dataValue
     $.ajax({
@@ -65,20 +71,17 @@ $(document).ready(function() {
         for (var i = 0; i < response.length; i++) {
           dataArray.push(response[i]);
 
-          // Now we have actions depending on which dataset was chosen.
+        // Now we have actions depending on which dataset was chosen.
           // If it the data is the ***DRUG DEATHS*** data
           if (response[i].calendar_year) {
+
             // Store the year values in the years array for the Time Buttons
             years.push(response[i].calendar_year);
-            // console.log("YEARS ARRAY: " + years);
-            // console.log("+++++++++++++++++++++++++++++++");
-            // console.log("TOTAL YEARS ARRAY: "+years);
-            // console.log("+++++++++++++++++++++++++++++++");
 
             // Add buttons to bottom left for going through time choices
             makeTimeButtons();
 
-            // Start by changing map options for location and zoom
+            // Set the map options for location and zoom
             options = {
               zoom: 8,
               // Baltimore, MD
@@ -87,7 +90,6 @@ $(document).ready(function() {
                 lng: -76.6122
               }
             }
-
 
             // Next, store values for simplicity/clarity
             // get the latitude and longitude values for Baltimore
@@ -101,9 +103,6 @@ $(document).ready(function() {
               }
             };
 
-            var deaths = response[i].alcohol_deaths;
-
-
             // These will be the values of deaths for the corresponding year only
             // See global var clickedYear for correct year
             // This value changes when the user clicks a Time Button
@@ -116,18 +115,7 @@ $(document).ready(function() {
               var opiod = response[i].prescription_opiod_deaths;
             }
 
-            console.log("+++++++++++++++++++++++++++++++");
-            console.log("Current Year: " + response[i].calendar_year);
-            console.log("Clicked Year: " + clickedYear);
-            console.log("1. Alcohol: " + alcohol);
-            console.log("2. Benzos: " + benzo);
-            console.log("3. Cocaine: " + cocaine);
-            console.log("4. Fentanyl: " + fentanyl);
-            console.log("5. Heroin: " + heroin);
-            console.log("6. Prescription Opiod: " + opiod);
-
-            console.log("+++++++++++++++++++++++++++++++");
-
+            // Create the infoWindow content
             var deathsPopUp = {
               popUp: '<div id="pop">' +
                 '<div class="popTitle">Overdose Deaths in Maryland in ' + clickedYear + '</div>' +
@@ -141,11 +129,10 @@ $(document).ready(function() {
                 '</ul>' +
                 '</div>'
             };
+
             // add each object to the json object
             $.extend(response[i], deathsPopUp, latLong);
           }
-
-
 
           // Else, if the data was the ***DRUNK DRIVING*** data...
           else if (response[i].state && response[i].alcohol_impaired_driving_deaths) {
@@ -163,6 +150,7 @@ $(document).ready(function() {
             // Next, store values for simplicity/clarity
             var deaths = response[i].alcohol_impaired_driving_deaths;
             var stName = response[i].state_state;
+
             // get the latitude and longitude values
             var x = response[i].state.coordinates[1];
             var y = response[i].state.coordinates[0];
@@ -174,9 +162,9 @@ $(document).ready(function() {
                 lng: y
               }
             };
-            // object for onclick pop up window
+            // infoWindow content 
             var deathsPopUp = {
-              popUp: "<div id='pop'><div class='popTitle'>" + stName + "</div><p> had " + deaths + " drunk driver deaths</p></div>"
+              popUp: "<div id='pop'><div class='popTitle'>" + stName + "</div><p>" + deaths + " drunk driver deaths</p></div>"
             };
             // object for the marker labels
             var deathsLabel = {
@@ -187,29 +175,19 @@ $(document).ready(function() {
             $.extend(response[i], deathsLabel, deathsPopUp, latLong);
           }
 
-
-
-
-
-
-
-
-
           // Else, if it the data is the ***WW POP*** data
           else if (response[i].countryName && response[i].popData) {
-
+            
+            years = [];
+            
             // Store the year values in the years array for the Time Buttons
             for (var j = 1990; j < 2015; j++) {
-              years.push(response[i].popData.total[j]);
-              console.log("YEARS ARRAY: " + years);
-              console.log("+++++++++++++++++++++++++++++++");
-              console.log("TOTAL YEARS ARRAY: " + years);
-              console.log("+++++++++++++++++++++++++++++++");
+
+              years.push(response[i].popData[j].year);
             }
 
             // Add buttons to bottom left for going through time choices
             makeTimeButtons();
-
 
             // Start by changing map options for location and zoom
             options = {
@@ -221,56 +199,29 @@ $(document).ready(function() {
               }
             }
 
-
             // Next, store values for simplicity/clarity
-            var totalPop = response[i].popData.total[year];
-            console.log("Total Pop for " + year + ": " + totalPop);
+            var country = response[i].countryName;
+            var children = response[i].popData[year].percent_0_to_14;
+            var adults = response[i].popData[year].percent_15_to_64;
+            var elderly = response[i].popData[year].percent_65_plus;
+            var totalPop = response[i].popData[year].total;
 
             // object for onclick pop up window
-            // var popPopUp = {popUp:
-            //   '<div>' +
-            //     '<h3>Overdose Deaths in Maryland in '+ clickedYear +'</h3>' +
-            //       '<ul>' +
-            //         '<li>Alcohol: '+ alcohol +'</li>' +
-            //         '<li>Benzodiazepine:  '+ benzo +'</li>' +
-            //         '<li>Cocaine:  '+ cocaine +'</li>' +
-            //         '<li>Fentanyl:  '+ fentanyl +'</li>' +
-            //         '<li>Heroin:  '+ heroin +'</li>' +
-            //         '<li>Prescription Opiod:  '+ opiod +'</li>' +
-            //       '</ul>' +
-            //   '</div>'};
-
-            // object for the marker labels
-            var popLabel = {
-              label: totalPop
-            };
+            var popPopUp = {popUp:
+              '<div id="pop">' +
+                '<h3 class="popTitle">Population breakdown for ' + country + ' in ' + year + ':</h3>' +
+                  '<ul>' +
+                    '<li>Total Population: '+ totalPop +'</li>' +
+                    '<li>Ages 0 to 14:  '+ children +'</li>' +
+                    '<li>Ages 15 to 64:  '+ adults +'</li>' +
+                    '<li>Ages 65+:  '+ elderly +'</li>' +
+                  '</ul>' +
+              '</div>'};
 
             // add each object to the json object
-            $.extend(response[i], popLabel, /*popPopUp*/ );
-
-
-            // Pseudo Code...think it out!
-            // 11 objects, 1 for a diff contry each
-            //   response[i].countryName
-            // start by working on the total pop values for 2014
-            //   would be great to later sort by years
-            //     response[i].popData.total.1990-2014
-            //   sort by the 3 age brackets
-            //     response[i].popData.percent_0_to_14
-            //     response[i].popData.percent_15_to_64
-            //     response[i].popData.percent_65_plus
-
+            $.extend(response[i], popPopUp);
 
           }
-
-
-
-
-
-
-
-
-
           console.log(response[i]);
         }
         initMap();
@@ -305,6 +256,7 @@ $(document).ready(function() {
     console.log(years);
     console.log(dataArray);
     clickedYear = years;
+    year = years;
     for (var i = 0; i < dataArray.length; i++) {
       // Now we have actions depending on which dataset was chosen.
       // If it the data is the ***DRUG DEATHS*** data
@@ -323,18 +275,6 @@ $(document).ready(function() {
           var opiod = dataArray[i].prescription_opiod_deaths;
         }
 
-        console.log("+++++++++++++++++++++++++++++++");
-        console.log("Current Year: " + dataArray[i].calendar_year);
-        console.log("Clicked Year: " + clickedYear);
-        console.log("1. Alcohol: " + alcohol);
-        console.log("2. Benzos: " + benzo);
-        console.log("3. Cocaine: " + cocaine);
-        console.log("4. Fentanyl: " + fentanyl);
-        console.log("5. Heroin: " + heroin);
-        console.log("6. Prescription Opiod: " + opiod);
-
-        console.log("+++++++++++++++++++++++++++++++");
-
         var deathsPopUp = {
           popUp: '<div id="pop">' +
             '<div class="popTitle">Overdose Deaths in Maryland in ' + clickedYear + '</div>' +
@@ -351,6 +291,31 @@ $(document).ready(function() {
         // add each object to the json object
         $.extend(dataArray[i], deathsPopUp);
       }
+
+      else if (dataArray[i].countryName && dataArray[i].popData) {
+        
+        var country = dataArray[i].countryName;
+        var children = dataArray[i].popData[year].percent_0_to_14;
+        var adults = dataArray[i].popData[year].percent_15_to_64;
+        var elderly = dataArray[i].popData[year].percent_65_plus;
+        var totalPop = dataArray[i].popData[year].total;
+
+        // object for onclick pop up window
+        var popPopUp = {popUp:
+          '<div id="pop">' +
+            '<h3 class="popTitle">Population breakdown for ' + country + ' in ' + year + ':</h3>' +
+              '<ul>' +
+                '<li>Total Population: '+ totalPop +'</li>' +
+                '<li>Ages 0 to 14:  '+ children +'</li>' +
+                '<li>Ages 15 to 64:  '+ adults +'</li>' +
+                '<li>Ages 65+:  '+ elderly +'</li>' +
+              '</ul>' +
+          '</div>'};
+
+        // add each object to the json object
+        $.extend(dataArray[i], popPopUp);
+
+      }
   }
   initMap();
 }
@@ -359,7 +324,7 @@ $(document).ready(function() {
 
 
 
-  //**************GOOGLE MAP**************//
+//**************GOOGLE MAP**************//
   function initMap() {
     // new map
     var map = new google.maps.Map(document.getElementById("map"), options);
@@ -406,7 +371,7 @@ $(document).ready(function() {
 
   google.maps.event.addDomListener(window, "load", initMap);
 
-  google.maps.event.addListener(infowindow, 'domready', function() {
+google.maps.event.addListener(infowindow, 'domready', function() {
 
    // Reference to the DIV which receives the contents of the infowindow using jQuery
    var iwOuter = $('.gm-style-iw');
